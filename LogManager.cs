5edea2a27d54bf64d1202034ffb6977f6ff1e1ee
gouzhun <suntabu@163.com>
2016-11-21 15:35:36 +0800

    first commit

---
+++ LogManager.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using Inlycat;
+using UnityEngine;
+
+namespace Suntabu
+{
+
+    public class SunLog
+    {
+        public static void Log(string msg)
+        {
+            LogManager.Instance.Log(msg);
+        }
+    }
+
+    /// <summary>
+    /// TODO:
+    /// 1. use editor to display each module's log file 
+    /// 2. use editor to config each module's opening state
+    /// 3. in log editor, we can add new module(even developer's name) to debug 
+    /// 
+    /// 
+    /// </summary>
+    internal class LogManager : Singleton<LogManager>
+    {
+        private StringBuilder mSb;
+        private bool mEnable = true;
+
+        private LogManager()
+        {
+            mSb = new StringBuilder(1000);
+        }
+
+        public void Log(string msg)
+        {
+            string log = string.Format("{0} : {1}", DateTime.Now.ToLongTimeString(), msg);
+            mSb.Append(log);
+            if (mEnable)
+            {
+                Debug.Log(log);
+            }
+        }
+
+    }
+
+
+}
